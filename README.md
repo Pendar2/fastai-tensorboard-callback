01809677b1976b041ad3680ed3d6cdb1ce0f5dcf
Pendar2 <bryanheff14@gmail.com>
2018-10-19 06:34:33 -0400

    Updated for fastai v1

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Fast.ai Tensorboard Callback
 
+Updated to support Fastai v1
+
+[Fastai forum post](https://forums.fast.ai/t/tensorboard-callback-for-fastai/19048)
+
+This callback plots training loss, validation loss, metrics, learning rate, and momentum. Every X iterations a snapshot of the model’s weights are logged and can be viewed in Tensorboard histogram and distribution tab. Every epoch, the embedding layers are saved and can be viewed in 3D with dimensionality reduction in the projector tab. Lastly, the model’s dataflow graph can be viewed in the graph tab.
+
 Requirements:
-* `pip install git+https://github.com/lanpa/tensorboard-pytorch`
-* `pip install tensorflow`
-* Pytorch >= 0.4 for graph visualization
+* `pip install tensorboard`
+* `pip install tensorboardX`
+
+
+Launch the Tensorboard server with `tensorboard --logdir="learn.path/logs"`, then navigate to localhost:6006
+
+Files with _old are for v0.* of fastai.
--- tensorboard_cb.py
+++ tensorboard_cb.py
@@ -1,49 +1,46 @@
 from tensorboardX import SummaryWriter
-from fastai.sgdr import *
+from fastai import *
 
+@dataclass
 class TensorboardLogger(Callback):
-    def __init__(self, model, md, log_name, metrics_names=[], path=None, histogram_freq=100):
-        super().__init__()
-        self.model = model
-        self.md = md
-        self.metrics_names = ["validation_loss"]
-        self.metrics_names += metrics_names
-        self.histogram_freq = histogram_freq
-        
-        path = path or os.path.join(md.path, "logs")
-        self.log_dir = os.path.join(path, log_name)
-        
-    def on_train_begin(self):
-        self.iteration = 0
-        self.epoch = 0
+    learn:Learner
+    run_name:str
+    histogram_freq:int=100
+    path:str=None
+    def __post_init__(self):
+        self.path = self.path or os.path.join(self.learn.path, "logs")
+        self.log_dir = os.path.join(self.path, self.run_name)
+    def on_train_begin(self, **kwargs):
         self.writer = SummaryWriter(log_dir=self.log_dir)
-    def on_batch_begin(self): pass
-    def on_phase_begin(self): pass
-    def on_epoch_end(self, metrics):
-        self.epoch += 1
+    def on_epoch_end(self, **kwargs):
+        iteration = kwargs["iteration"]
+        metrics = kwargs["last_metrics"]
+        metrics_names = ["valid_loss"] + [o.__name__ for o in self.learn.metrics]
         
-        for val, name in zip(metrics, self.metrics_names):
-            self.writer.add_scalar(name, val, self.iteration) 
-        
-        
-        for name, emb in self.model.named_children():
+        for val, name in zip(metrics, metrics_names):
+            self.writer.add_scalar(name, val, iteration)
+            
+        for name, emb in self.learn.model.named_children():
             if isinstance(emb, nn.Embedding):
-                self.writer.add_embedding(list(emb.parameters())[0], global_step=self.iteration, tag=name)
+                self.writer.add_embedding(list(emb.parameters())[0], global_step=iteration, tag=name)
                 
-    def on_phase_end(self): pass
-    def on_batch_end(self, loss):
-        self.iteration += 1
-        self.writer.add_scalar("loss", loss, self.iteration)
+    def on_batch_end(self, **kwargs):
+        iteration = kwargs["iteration"]
+        loss = kwargs["last_loss"]
         
-        if self.iteration%self.histogram_freq==0:
-            for name, param in self.model.named_parameters():
-                self.writer.add_histogram(name, param, self.iteration)
-    def on_train_end(self):
+        self.writer.add_scalar("learning_rate", self.learn.opt.lr, iteration)
+        self.writer.add_scalar("momentum", self.learn.opt.mom, iteration)
+        
+        self.writer.add_scalar("loss", loss, iteration)
+        if iteration%self.histogram_freq==0:
+            for name, param in self.learn.model.named_parameters():
+                self.writer.add_histogram(name, param, iteration)
+    def on_train_end(self, **kwargs):
         try:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
-                dummy_input = tuple(next(iter(self.md.trn_dl))[:-1])
-                self.writer.add_graph(self.model, dummy_input)
+                dummy_input = next(iter(self.learn.data.train_dl))[0]
+                self.writer.add_graph(self.learn.model, tuple(dummy_input))
         except Exception as e:
             print("Unable to create graph.")
             print(e)
